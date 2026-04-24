e708ed07e44ff5710d9003a5fd634132f25200c8
mekefly <mekefly@outlook.com>
2026-04-24 12:51:24 +0800

    Release 1.0.2 (https://github.com/mekefly/colors/pull/4)
    * fix: 修复复制时通知的错误
    
    * feat: 优化，使颜色轮盘更好用
    
    * fix: hsv中的v写死的bug，色盘颜色对比度不足的问题
    
    * chore: 删除遗留的console.log
    
    * chore: 删除一些暂时没用启用的代码

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,4 +1,4 @@
-import { showSuccess } from "./notification";
+import { showError, showSuccess } from "./notification";
 
 export const copyColor = async (text: string) => {
   try {
@@ -6,6 +6,6 @@ export const copyColor = async (text: string) => {
     showSuccess(`已复制颜色: ${text}`);
   } catch (err) {
     console.error("复制失败:", err);
-    showSuccess("复制失败");
+    showError("复制失败");
   }
 };
